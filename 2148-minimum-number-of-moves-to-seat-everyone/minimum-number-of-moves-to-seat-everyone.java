c4039c043759e3c56a542c70f5828ee20ff18b22
AadhilMalim <112715361+AadhilMalim@users.noreply.github.com>
2024-06-13 20:37:28 +0530

    Time: 2 ms (97.35%) | Memory: 44 MB (49.16%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -3,25 +3,13 @@ class Solution {
         int len = seats.length;
         // if(len==1) return 0;
 
-        for(int i=0; i<len; i++){
-            for(int j=i+1;j<len; j++){
-                if(seats[j]<seats[i]){
-                    int temp = seats[i];
-                    seats[i] = seats[j];
-                    seats[j] = temp;
-                }
-                if(students[j]<students[i]){
-                    int temp = students[i];
-                    students[i] = students[j];
-                    students[j] = temp;
-                }
-            }
-        }
+        Arrays.sort(seats);
+        Arrays.sort(students);
         int swaps = 0;
-        int first = students[0] - seats[0];
-        if(first<0) swaps+= first*-1; else swaps+=first;
-        for(int i=1; i<len  ;i++){
-            first= students[i] - seats[i];
+        
+        
+        for(int i=0; i<len  ;i++){
+           int  first= students[i] - seats[i];
                 if(first<0) swaps+= first*-1; else swaps+=first;
         }
 
