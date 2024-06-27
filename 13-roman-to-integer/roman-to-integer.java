5996bc3a60779336fd2e558d19328f3b7c7098bc
AadhilMalim <112715361+AadhilMalim@users.noreply.github.com>
2024-06-27 19:33:41 +0530

    Time: 6 ms (30.53%) | Memory: 44.6 MB (40.85%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,7 +2,7 @@ class Solution {
     public int romanToInt(String s) {
         int sum =0;
 		
-		Map<Character, Integer> mymap1 = new TreeMap<Character, Integer>();
+		Map<Character, Integer> mymap1 = new HashMap<Character, Integer>();
 		mymap1.put('I', 1);
 		mymap1.put('V', 5);
 		mymap1.put('X', 10);
@@ -11,12 +11,12 @@ class Solution {
 		mymap1.put('D', 500);
 		mymap1.put('M', 1000);
 		
-		char[] given = s.toCharArray();
-		int len = given.length;
+		
+		int len = s.length();
         
        while(len>1){
-            int b1 = mymap1.get(given[len-1]);
-            int b2 = mymap1.get(given[len-2]);
+            int b1 = mymap1.get(s.charAt(len-1));
+            int b2 = mymap1.get(s.charAt(len-2));
             if( b1 > b2){
                 sum+=b1-b2;
                 len-=2;
@@ -27,7 +27,7 @@ class Solution {
         }
         if(len==1)
 		{
-			sum+=mymap1.get(given[0]);
+			sum+=mymap1.get(s.charAt(0));
 		}
 		return sum;
     }
